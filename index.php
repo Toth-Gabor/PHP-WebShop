4fbcb5dc49eacd3cc29f2c2547dbb06bbb762408
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-09-19 11:05:05 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+// core configuration
+include_once "config/core.php";
+
+// set page title
+$page_title="Index";
+
+
+// include page header HTML
+include_once 'layout_head.php';
+
+echo "<div class='col-md-12'>";
+    // content once logged in
+    echo "<div class='alert alert-info'>";
+        echo "Content when logged in will be here. For example, your premium products or services.";
+    echo "</div>";
+
+echo "</div>";
+
+// footer HTML and JavaScript codes
+include 'layout_foot.php';
+?>
---
+++ logger/login_checker.php
@@ -0,0 +1,26 @@
+<?php
+// login checker for 'customer' access level
+
+// if access level was not 'Admin', redirect him to login page
+if(isset($_SESSION['access_level']) && $_SESSION['access_level']=="Admin"){
+    header("Location: {$home_url}admin/index.php?action=logged_in_as_admin");
+}
+elseif (isset($_SESSION['access_level']) && $_SESSION['access_level']=="Customer"){
+    header("Location: {$home_url}customer/index.php?action=already_logged_in");
+}
+
+// if $require_login was set and value is 'true'
+else if(isset($require_login) && $require_login==true){
+    // if user not yet logged in, redirect to login page
+    if(!isset($_SESSION['access_level'])){
+        header("Location: {$home_url}logger/login.php?action=please_login");
+    }
+}
+
+// if it was the 'login' or 'register' or 'sign up' page but the customer was already logged in
+else if(isset($page_title) && ($page_title=="Login" || $page_title=="Sign Up")){
+    // if user not yet logged in, redirect to login page
+    if(isset($_SESSION['access_level']) && $_SESSION['access_level']=="Customer"){
+        header("Location: {$home_url}index.php?action=already_logged_in");
+    }
+}
