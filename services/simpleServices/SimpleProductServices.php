060c29b3df1f4e09bd502bb73eaad7483d164c69
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-09-22 17:51:02 +0200

    Added product.php

---
+++ services/simpleServices/SimpleProductServices.php
@@ -0,0 +1,25 @@
+<?php
+
+include_once "services/ProductServices.php";
+include_once "Database/Dao/DatabaseProductDao.php";
+
+class SimpleProductServices implements ProductServices {
+
+    private $productDao;
+
+    public function __construct()
+    {
+        $this->productDao = new DatabaseProductDao();
+    }
+
+
+    public function ReadAll()
+    {
+        return $this->productDao->GetAll();
+    }
+
+    public function ReadOne()
+    {
+        // TODO: Implement ReadOne() method.
+    }
+}
