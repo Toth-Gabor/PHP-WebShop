1122a611f7711447507b9763b903f19a6fb6a505
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-09-22 17:30:30 +0200

    Added AbstractDao

---
+++ Database/Dao/AbstractDao.php
@@ -0,0 +1,25 @@
+<?php
+
+
+abstract class AbstractDao
+{
+    private $host = "localhost";
+    private $db_name = "php_login_system";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    // get the database connection
+    public function getConnection(){
+
+        $this->conn = null;
+
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+        //echo "Connected successfully";
+        return $this->conn;
+    }
+}
