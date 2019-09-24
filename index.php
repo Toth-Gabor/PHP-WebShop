cd6bcc907521313185c2a8991e4438e112c9d375
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-09-24 17:53:37 +0200

    Updated index.php

--- index.php
+++ index.php
@@ -9,7 +9,23 @@ $page_title="Index";
 // include page header HTML
 include_once 'layout_head.php';
 
+$action = isset($_GET['action']) ? $_GET['action'] : "";
+echo "<div class='col-md-12'>";
+if($action=='added'){
+    echo "<div class='alert alert-info'>";
+    echo "Product was added to your cart!";
+    echo "</div>";
+}
+
+if($action=='exists'){
+    echo "<div class='alert alert-info'>";
+    echo "Product already exists in your cart!";
+    echo "</div>";
+}
+echo "</div>";
+
 include_once "all_products_template.php";
+
 echo "</div>";
 
 // footer HTML and JavaScript codes
