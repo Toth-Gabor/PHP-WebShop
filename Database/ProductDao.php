b1bec5372c25d64f3b8a2c777eef2a3ed74f638e
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-09-24 17:52:02 +0200

    Added new functions

--- Database/ProductDao.php
+++ Database/ProductDao.php
@@ -5,5 +5,8 @@ interface ProductDao
 {
     public function GetAll();
     public function GetOneById($product_id);
+    public function GetAllByIds($productIdList);
     public function GetAllByCategory($category);
+    public function DecrementQuantity($quantity);
+    public function IncrementQuantity($quantity);
 }
