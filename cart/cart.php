467d8c63a312cc0f00b7123bea6b6600abcc5c4b
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-09-24 17:51:12 +0200

    Updated cart.php

--- cart/cart.php
+++ cart/cart.php
@@ -3,12 +3,41 @@ include_once "../config/core.php";
 
 $page_title = "Cart";
 
-include_once "../cart/layout_head.php";
+include_once __DIR__ . "/../cart/layout_head.php";
+include_once __DIR__ . "/../services/simpleServices/SimpleProductServices.php";
+
+$action = isset($_GET['action']) ? $_GET['action'] : "";
+
+echo "<div class='col-md-12'>";
+        if($action=='removed'){
+            echo "<div class='alert alert-info'>";
+                echo "Product was removed from your cart!";
+            echo "</div>";
+        }
+
+        else if($action=='quantity_updated'){
+            echo "<div class='alert alert-info'>";
+                echo "Product quantity was updated!";
+            echo "</div>";
+        }
+echo "</div>";
+
 if (!isset($_SESSION['cart']) || ($_SESSION['cart'] == 0)){?>
     <div class='col-md-12'>
         <div class='alert alert-danger'>No products found in your cart</div>
     </div>
     <?php
+} else{
+    $ids = array();
+    foreach($_SESSION['cart'] as $id=>$value ){
+        array_push($ids, $id);
+    }
+    var_dump($ids);
+    var_dump($_SESSION['cart']);
+
+    $quantity=$_SESSION['cart'][1]['quantity'];
+    //$sub_total=$price*$quantity;
+
 }
 include '../layout_foot.php';
 ?>
