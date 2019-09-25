a7327555e07ddccd9936c46ed8cbb16fda893e1f
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-09-25 14:48:11 +0200

    Added update_cart.php

--- config/init.sql
+++ config/init.sql
@@ -23,7 +23,7 @@ CREATE TABLE products
     brand         TEXT           NOT NULL,
     specification TEXT           NOT NULL,
     description   TEXT           NOT NULL,
-    price         DECIMAL(10, 2) NOT NULL,
+    price         INT (11)       NOT NULL,
     quantity      INTEGER        NOT NULL,
     image         TEXT,
     category      TEXT           NOT NULL
@@ -42,19 +42,19 @@ INSERT INTO `users` (`firstname`, `lastname`, `email`, `contact_number`, `addres
 ('Tony', 'Parker', 'tony@example.com', '8888888', 'Blk 24 A Lot 6 Ph 3\r\nPeace Village, San Luis', '$2y$10$lBJfvLyl/X5PieaztTYADOxOQeZJCqETayF.O9ld17z3hcKSJwZae', 'Customer', 'THM3xkZzXeza5ISoTyPKl6oLpVa88tYl', 1, '2016-05-26 01:29:01', '2016-06-13 17:46:33');
 
 INSERT INTO products (product_name, brand, specification, description, quantity, price, image, category)
-VALUES ('EF 50mm F1.8 STM lens', 'Canon',
+VALUES ('Canon EF 50mm F1.8 STM lens', 'Canon',
         'Mount: Canon EF | Elements/groups: 6/5 | Diaphragm blades: 7 | Autofocus: Stepping motor | Stabilizer: None | Minimum focus distance: 0.35m | Maximum magnification: 0.21x | Filter thread: 49mm | Dimensions (WxL): 69x39mm | Weight: 160g',
         'A great low-cost portrait lens for APS-C Canon DSLRs',
         10, 114, 'photos/Canon_EF_50mm_F1.8_STM_lens.jpg', 'Lens'),
-       ('EF-S 17-55mm f2.8 IS USM', 'Canon',
+       ('Canon EF-S 17-55mm f2.8 IS USM', 'Canon',
         'Mount: Canon EF-S | Effective zoom range: 27-88mm | Lens construction: 19 elements in 12 groups | No. of diaphragm blades: 7 blades | Minimum focus distance: 0.35m | Filter size: 77mm | Dimensions: 84x111mm | Weight: 645g',
         'A veteran APS-C format lens, with high-quality build',
         11, 765, 'photos/Canon_EF-S_17-55mm_f2.8_IS_USM.jpg', 'Lens'),
-       ('EF 24-70mm f2.8L II USM', 'Canon',
+       ('Canon EF 24-70mm f2.8L II USM', 'Canon',
         'Mount: Canon EF | Effective zoom range: 24-70mm | Lens construction: 18 elements in 13 groups | No. of diaphragm blades: 9 blades | Minimum focus distance: 0.38m | Filter size: 82mm | Dimensions: 89x113mm | Weight: 805g',
         'This Mk II edition is the reinvention of a classic',
         20, 1789, 'photos/Canon_EF_24-70mm_f2.8L_II_USM.jpg', 'Lens'),
-       ('EF 24-105mm f4L IS II USM', 'Canon',
+       ('Canon EF 24-105mm f4L IS II USM', 'Canon',
         'Mount: Canon EF | Effective zoom range: 24-105mm | Lens construction: 17 elements in 12 groups | No. of diaphragm blades: 10 blades | Minimum focus distance: 0.45m | Filter size: 77m | Dimensions: 84x118mm | Weight: 795g',
         'Another Canon standard zoom gets upgraded',
         5, 1299, 'photos/Canon_EF_24-105mm_f4L_IS_II_USM.jpg', 'Lens'),
