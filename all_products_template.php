dccc77bbb43e3a4aa7669ba3bd03df76cf84fe9a
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-09-26 13:11:58 +0200

    Updated th elements and style

--- all_products_template.php
+++ all_products_template.php
@@ -19,10 +19,10 @@ $productsList = $SAPS->ReadAll();
             <th><strong>Description</strong></th>
             <th><strong>Price</strong></th>
             <th><strong>Stock</strong></th>
-            <th><strong>Image Url</strong></th>
+            <!--<th><strong>Image Url</strong></th>-->
             <th><strong>Category</strong></th>
-            <th><strong>Select</strong></th>
-            <th><strong>Add to Cart</strong></th>
+            <th><strong>Actions</strong></th>
+            <!--<th><strong>Add to Cart</strong></th>-->
         </tr>
         <?php foreach ($productsList as $product){
             ?>
@@ -34,12 +34,12 @@ $productsList = $SAPS->ReadAll();
                 <td><?= $product->getDescription() ?></td>
                 <td>$<?= $product->getPrice() ?></td>
                 <td><?= $product->getQuantity() ?></td>
-                <td><?= $product->getImage() ?></td>
+                <!--<td><?= $product->getImage() ?></td>-->
                 <td><?= $product->getCategory() ?></td>
-                <td><?= "<a href='product.php?id={$product->getId()}' class='btn btn-labeled btn-success'>Select</a></td>"?>
-                <td><a href="cart/add_to_cart.php?id=<?= $product->getId() ?>" type="button" class="btn btn-labeled btn-success">
+                <td><?= "<a href='product.php?id={$product->getId()}' class='btn btn-labeled btn-info'>Select</a>"?>
+                <a href="cart/add_to_cart.php?id=<?= $product->getId() ?>" type="button" class="margin-1em-zero btn btn-labeled btn-success mt-5px">
                     <span class="btn-label"></span>Add to cart</a>
-                </td>
+
             </tr>
         <?php
         }
