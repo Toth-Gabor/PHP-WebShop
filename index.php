d9c93d3d24d55190594f3d5a0570ee6e28510d20
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-09-28 13:26:23 +0200

    Added "Add" function

--- index.php
+++ index.php
@@ -11,25 +11,26 @@ include_once 'layout_head.php';
 $action = isset($_GET['action']) ? $_GET['action'] : "";
 echo "<div class='col-md-12'>";
 
+// alert if item added to cart
 if($action=='added'){?>
     <script type="text/javascript">
         swal({title:'Success', text:'This item has been added to Cart!', type:'success', timer:1700});
     </script> <?php
 }
 
+// alert if item already added to cart
 if($action=='exists'){?>
     <script type="text/javascript">
         swal({title:'Warning', text:'This item already exists in your Cart!', type:'warning', timer:1700});
     </script> <?php
 }
+
+// alert if items purchased
 if($action=='purchased') {?>
     <script type="text/javascript">
-        swal({title:'Success', text:'Thank you for your purchase! You can lay down, your products will arrive soon!', type:'success', timer:2000);
+        swal({title:'Success', text:'Thank you for your purchase! You can lay down, your products will arrive soon!', type:'success', timer:3000});
     </script> <?php
 
-    $order = json_encode($_SESSION['cart']);
-    var_dump($order);
-    unset($_SESSION['cart']);
 }
 include_once "all_products_template.php";
 
