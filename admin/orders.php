4d119cb9eeb2387c7222d744df6f81bffe9a5018
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-09-29 10:44:35 +0200

    Added order.php

---
+++ admin/orders.php
@@ -0,0 +1,14 @@
+<?php
+$page_title="Orders";
+
+include_once "../config/core.php";
+
+include_once "admin_layout_head.php";
+
+echo "<div class='container'>";
+
+include_once "all_orders_template.php";
+
+echo "</div>";
+
+include_once "../layout_foot.php";
--- admin/teszt.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo "teszt php oldal";
