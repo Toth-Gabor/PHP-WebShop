b31f1585666a9092bdd70904ffcd3e933243c2ad
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-09-29 15:02:12 +0200

    Added UserServices

---
+++ services/UserServices.php
@@ -0,0 +1,11 @@
+<?php
+
+
+interface UserServices
+{
+    public function AddUser($firstname, $lastname, $email, $contact_number, $address, $password,
+                               $access_level, $access_code, $status, $created);
+    public function ReadUserById($user_id);
+    public function ReadAllUser();
+    public function CheckEmailExists($email);
+}
