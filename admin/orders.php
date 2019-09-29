1b7c43e7ec20bee7d4b8e89ccbb77dcb459bd38b
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-09-29 13:20:58 +0200

    Added alerts to orders.php

--- admin/orders.php
+++ admin/orders.php
@@ -5,10 +5,30 @@ include_once "../config/core.php";
 
 include_once "admin_layout_head.php";
 
+include_once "../layout_foot.php";
+
+$action = isset($_GET['action']) ? $_GET['action'] : "";
+
+// alert if order was deleted
+if($action=='deleted') {?>
+
+    <script type="text/javascript">
+        swal({title: 'Deleted', text: 'The selected order has been Deleted!', type: 'info', timer: 1800});
+    </script> <?php
+}
+
+if ($action=='error') {?>
+
+    <script type="text/javascript">
+        swal({title: 'Error', text: 'The selected order has NOT been Deleted!', type: 'error', timer: 1800});
+    </script> <?php
+}
+
+
+
 echo "<div class='container'>";
 
 include_once "all_orders_template.php";
 
 echo "</div>";
 
-include_once "../layout_foot.php";
