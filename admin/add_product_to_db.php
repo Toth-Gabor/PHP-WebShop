8782e0c706447378b638cf883f1b7d05ddd2c8a6
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-10-02 17:06:19 +0200

    Updated add_product_to_db.php

--- admin/add_product_to_db.php
+++ admin/add_product_to_db.php
@@ -1,5 +1,8 @@
 <?php
 include_once "../services/simpleServices/SimpleProductServices.php";
+include_once "../libs/php/utils.php";
+$ulils = new Utils();
+
 $product_srv = new SimpleProductServices();
 // get info from post
 $name = $_POST['name'];
@@ -8,10 +11,20 @@ $specification = $_POST['specification'];
 $description = $_POST['description'];
 $price = $_POST['price'];
 $quantity = $_POST['stock'];
-$image = $_POST['image'];
+$image=!empty($_FILES["image"]["name"])
+    ? sha1_file($_FILES['image']['tmp_name']) . "-" . basename($_FILES["image"]["name"]) : "";
+$message = $ulils->uploadPhoto($image);
+
 $category = $_POST['category'];
 
-// add to database
-$product_srv->CreateProduct($name, $brand, $specification, $description, $price, $quantity, $image, $category);
+if ($message == ""){
+    // add to database
+    $imagePath = $_SESSION['filepath'];
+    $product_srv->CreateProduct($name, $brand, $specification, $description, $price, $quantity, $imagePath, $category);
 
-header('Location: ../admin/index.php?action=added');
+    header('Location: ../admin/index.php?action=added');
+} else {
+    header('Location: ../admin/create_product.php?message=' . $message);
+}
+
+
