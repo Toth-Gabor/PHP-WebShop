0accdde7fe010ab09cbcda01eac2dc11451802db
Gabor Toth <44711404+Toth-Gabor@users.noreply.github.com>
2019-10-09 09:33:38 +0200

    Updated show products style

--- new_all_products.php
+++ new_all_products.php
@@ -3,9 +3,8 @@
 global $product_srv;
 global $category;
 
-$hide_add_button = "";
 
-if ($category != ""){
+if ($category != "") {
     // get category filtered products
     $productsList = $product_srv->ReadAllByCategory($category);
 } else {
@@ -16,29 +15,61 @@ if ($category != ""){
 echo "<div class='container-fluid'>";
 
 foreach ($productsList as $product) {
+    $hide_add_button = "";
+    $button_text = "Add to Cart";
     // hide add button if quantity is 0
-    if ($product->getQuantity() == 0) {
+    $quantity = $product->getQuantity();
+    if ($quantity == 0) {
         $hide_add_button = "disabled";
+        $button_text = "Out Of Stock!";
     }
     ?>
-    <div class="col-sm-3" style="border: royalblue">
-        <div class="thumb-wrapper">
-            <div class="img-box mx-auto">
+
+
+    <div class="col-md-4">
+        <figure class="product-card">
+            <div class="img-container">
                 <img onerror="this.src='uploads/default.jpg'" src='<?= $product->getImage() ?>'
                      class="img-responsive img-fluid" alt=""/>
             </div>
+            <figcaption class="info-wrap pc-title">
+                <h4 class="title"><?= $product->getName() ?></h4>
+                <p class="desc"><?= $product->getDescription() ?></p>
+            </figcaption>
+            <div class="bottom-wrap">
+                <div class="price-wrap h5">
+                    <p class="item-price"><span>Price: $<?= $product->getPrice() ?>.00</span></p>
+                    <p class="item-price"><span>In Stock: <?= $product->getQuantity() ?></span></p>
+                </div>
+                <div class="pc-buttons">
+                    <a href='product.php?id=<?= $product->getId() ?>' class='btn btn-labeled btn-info btn-sm m-0'>Details</a>
+                    <a href="cart/add_to_cart.php?id=<?= $product->getId() ?>" type="button"
+                       class="<?= $hide_add_button ?>  btn btn-labeled btn-success btn-sm m-0">
+                        <span class="btn-label"></span><?= $button_text ?></a>
+                </div>
+            </div>
+        </figure>
+    </div>
+
+    <!--<div class="col-sm-3" style="border: royalblue">
+        <div class="thumb-wrapper">
+            <div class="img-box mx-auto">
+                <img onerror="this.src='uploads/default.jpg'" src='<?/*= $product->getImage() */ ?>'
+                     class="img-responsive img-fluid" alt="" style="width:100%; height: auto"/>
+            </div>
             <div class="thumb-content">
-                <h4><?= $product->getName() ?></h4>
-                <p class="item-price"><span>Price: $<?= $product->getPrice() ?>.00</span></p>
+                <h4><?/*= $product->getName() */ ?></h4>
+                <p class="item-price"><span>Price: $<?/*= $product->getPrice() */ ?>.00</span></p>
+                <p class="item-price"><span>In Stock: <?/*= $product->getQuantity() */ ?></span></p>
             </div>
             <div>
-                <a href='product.php?id=<?= $product->getId() ?>' class='btn btn-labeled btn-info btn-sm m-0'>Details</a>
-                <a href="cart/add_to_cart.php?id=<?= $product->getId() ?>" type="button"
-                   class="<?= $hide_add_button ?>  btn btn-labeled btn-success btn-sm m-0">
-                    <span class="btn-label"></span>Add to Cart</a>
+                <a href='product.php?id=<?/*= $product->getId() */ ?>' class='btn btn-labeled btn-info btn-sm m-0'>Details</a>
+                <a href="cart/add_to_cart.php?id=<?/*= $product->getId() */ ?>" type="button"
+                   class="<?/*= $hide_add_button */ ?>  btn btn-labeled btn-success btn-sm m-0">
+                    <span class="btn-label"></span><?/*= $button_text */ ?></a>
             </div>
         </div>
-    </div>
+    </div>-->
 
     <?php
 }
--- one_product_template.php
+++ one_product_template.php
@@ -8,36 +8,69 @@ $id = htmlspecialchars($_GET["id"]);
 $product = $product_srv->ReadOne($id);
 
 // check product stock
-if ($product->getQuantity() == 0){
+if ($product->getQuantity() == 0) {
     $block_add_to_cart = "disabled";
     $button_text = "Out of stock!";
-} else{
+} else {
     $block_add_to_cart = "";
     $button_text = "Add to cart";
 }
 ?>
-<div class="col-md-4">
+    <div class="product-card">
+        <div class="img-fluid float-left">
+            <!-- new card -->
+            <div class="img-container">
+                <img onerror="this.src='uploads/default.jpg'" src='<?= $product->getImage() ?>' style='width:300px;'/>
+            </div>
+        </div>
+        <!-- Right Column -->
+        <div class="pro">
 
-    <ul>
-        <li><strong> Id: </strong><?= $product->getId() ?></li>
-        <li><strong> Name: </strong><?= $product->getName() ?></li>
-        <li><strong>Brand: </strong><?= $product->getBrand() ?></li>
-        <li><strong>Specification: </strong><?= $product->getSpecification() ?></li>
-        <li><strong>Description: </strong><?= $product->getDescription() ?></li>
-        <li><strong>In Stock: </strong><?= $product->getQuantity() ?></li>
-        <li><strong>Price: </strong>$<?= $product->getPrice() ?>.00</li>
-        <!--<li><?= $product->getImage() ?></li>-->
-        <li><strong>Category: </strong><?= $product->getCategory() ?></li>
+            <!-- Product Description -->
+            <div class="product-description">
+                <span><?= $product->getCategory() ?></span>
+                <h1><?= $product->getName() ?></h1>
+                <p><?= $product->getDescription() ?></p>
+            </div>
 
-    </ul>
+            <!-- Product Pricing -->
+            <div class="product-price">
+                <span>$<?= $product->getPrice() ?>.00</span>
+                <a href="cart/add_to_cart.php?id=<?= $id ?>" type="button"
+                   class="<?= $block_add_to_cart ?> btn btn-labeled btn-success">
+                    <span class="btn-label"></span><?= $button_text ?></a>
+                <a href="index.php" type="button" class="btn btn-labeled btn-info">
+                    <span class="btn-label"></span>Back</a>
+            </div>
+        </div>
+        <div class="row width-80-percent">
+            <p><strong>Description: </strong><?= $product->getDescription() ?></p>
 
-</div>
+        </div>
+    </div>
     <div class="col-md-4">
-        <img onerror="this.src='uploads/default.jpg'" src='<?= $product->getImage() ?>' style='width:300px;' />
+
+        <ul>
+            <li><strong> Id: </strong><?= $product->getId() ?></li>
+            <li><strong> Name: </strong><?= $product->getName() ?></li>
+            <li><strong>Brand: </strong><?= $product->getBrand() ?></li>
+            <li><strong>Specification: </strong><?= $product->getSpecification() ?></li>
+            <li><strong>Description: </strong><?= $product->getDescription() ?></li>
+            <li><strong>In Stock: </strong><?= $product->getQuantity() ?></li>
+            <li><strong>Price: </strong>$<?= $product->getPrice() ?>.00</li>
+            <!--<li><?= $product->getImage() ?></li>-->
+            <li><strong>Category: </strong><?= $product->getCategory() ?></li>
+
+        </ul>
+
+    </div>
+    <div class="col-md-4">
+        <img onerror="this.src='uploads/default.jpg'" src='<?= $product->getImage() ?>' style='width:300px;'/>
     </div>
     <div class="col-md-12">
-        <a href="cart/add_to_cart.php?id=<?= $id ?>" type="button" class="<?= $block_add_to_cart?> btn btn-labeled btn-success">
-            <span class="btn-label"></span><?= $button_text?></a>
+        <a href="cart/add_to_cart.php?id=<?= $id ?>" type="button"
+           class="<?= $block_add_to_cart ?> btn btn-labeled btn-success">
+            <span class="btn-label"></span><?= $button_text ?></a>
         <a href="index.php" type="button" class="btn btn-labeled btn-info">
             <span class="btn-label"></span>Back</a>
     </div>
